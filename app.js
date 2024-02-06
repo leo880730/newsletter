8d438e633608e2a9f59e8050a48c3ee93d19847b
leo880730 <leov2880730@gmail.com>
2024-02-07 00:46:04 +0800

    Automatically send mail

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ submitBtn.addEventListener('click', function () {
   let address = document.querySelector("#mail-input").value
   let recipient = document.querySelector("#name-input").value
 
-  document.querySelector('#wait').style.display = "block"
+  document.querySelector('.loader').style.display = "block"
 
   $.ajax({
     //正式版
     // url: "",
 
     //測試版
-    url: "https://script.google.com/macros/s/AKfycbxZz_kcYOG6RFpUE6ui1lefGZC2JZGRtUBfUl5hnViluPBNYDQdT6GoTRWAu5jNVlZu9Q/exec",
+    url: "https://script.google.com/macros/s/AKfycbwb-Iqmp1_H5nHeuQ2uIo20OaGPgCZ5LRBX2zaIiloSsYzt8iEp2igO2tHzeOGpmAvYYA/exec",
 
     data: {
       "name": recipient,
@@ -26,9 +26,14 @@ submitBtn.addEventListener('click', function () {
         console.log('success')
         document.querySelector('#wait').style.display = "none"
         body.innerHTML = `
-          <div class="main-content">
-            <h1>訂閱成功啦！</h1>
-            <h2>稍後會收到一封確認信，如果沒收到的話可以翻翻垃圾郵件，或是你的信箱輸入錯了，重新輸入一次就好囉！</h2>
+          <div class="success-content">
+            <div class="success-container">
+              <h1 class="success-title">訂閱成功啦！</h1>
+              <h2 class="success-message">稍後會收到一封確認信，如果沒收到的話可以翻翻垃圾郵件，或是你的信箱輸入錯了，重新輸入一次就好囉！</h2>
+            </div>
+            <div class="btn-container">
+              <button class="learn-more-btn">Learn More　〉</button>
+            </div>
           </div>
         `
       }
@@ -40,7 +45,7 @@ submitBtn.addEventListener('click', function () {
       // document.querySelector('#submit-error-2').style.display = "block"
       body.innerHTML = `
           <div class="main-content">
-            <h1>系統錯誤，請稍後再試一次:( </h1>
+            <h1 class="error-message">系統錯誤，請稍後再試一次:( </h1>
           </div>
         `
       console.log('error')
@@ -48,3 +53,10 @@ submitBtn.addEventListener('click', function () {
   })
 })
 
+let learnMoreBtn = document.querySelector('.learn-more-btn')
+
+learnMoreBtn.addEventListener('click', function () {
+  body.innerHTML = `
+    <h1>home page</h1>
+  `
+})
